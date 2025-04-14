93804cc1212a5ba74a82a648fd85e2a5036cbd89
uakubue <uakubue@gmail.com>
2025-04-14 13:55:43 +0100

    New changes for error

--- src/main.jsx
+++ src/main.jsx
@@ -12,7 +12,7 @@ createRoot(document.getElementById('root')).render(
   <StrictMode>
     <BrowserRouter >
       <Provider store={store}>
-        <PersistGate loading={null} persistor={persistor}>
+        <PersistGate loading={true} persistor={persistor}>
           <App />
         </PersistGate>
       </Provider>
