acd506f68daa80445f995da10dd035d82546e9c8
uakubue <uakubue@gmail.com>
2025-04-23 13:54:48 +0100

    Updated styles in various pages

--- src/components/productDetails/ProductDetails.jsx
+++ src/components/productDetails/ProductDetails.jsx
@@ -1,16 +1,23 @@
 import React, { useEffect, useState } from 'react';
 import { Link, useParams } from 'react-router-dom';
 import CardLoaderPage from '../loadPage/cardLoader/CardLoaderPage';
+import "./ProductDetails.css"
+import { useDispatch } from 'react-redux';
+import { addItemToCart } from '../../CartSlice';
+import { FaNairaSign } from "react-icons/fa6";
+
 
 const ProductDetails = () => {
   const { id } = useParams(); // Get the product ID from the URL
   const [product, setProduct] = useState(null); // State to hold product details
+  const dispatch = useDispatch();
+  const [disabledProducts, setDisabledProducts] = useState([]);
 
   useEffect(() => {
     // Fetch product details using the ID
     const fetchProductDetails = async () => {
       try {
-        const response = await fetch(`https://fakestoreapi.com/products/${id}`); // Replace with your API endpoint
+        const response = await fetch(`https://fakestoreapi.com/products/${id}`); 
         const data = await response.json();
         setProduct(data); // Set the product details in state
       } catch (error) {
@@ -21,22 +28,46 @@ const ProductDetails = () => {
     fetchProductDetails();
   }, [id]); // Dependency array to re-run effect if id changes
 
+
+  const handleAddToCart = (product) => {
+    setDisabledProducts(prev => [...prev, product.id]); 
+    dispatch(addItemToCart(product));
+    // document.getElementById('navbar').style.display = 'block';
+  };
+
   return (
-    <div>
+    <div className='p_details_container'>
       {product ? (
-        <div>
-            <h3>{product.title}</h3>
-            <div>
+        <div className='p_details_div'>
+            
+            <div className='item_image_div'>
                 <img src={product.image} alt="" />
             </div>
-            <p>Price: {product.price}</p>
-            <p>{product.description}</p>
-            <span>{product.id}</span>
+            
+            <div className='item_details_div'>
+              <div className='p_info_details_div'>
+                <h2 className='product_title'>{product.title}</h2>
+                <span className='product_price'>
+                  <span><FaNairaSign /></span>
+                  <span>{product.price}</span>
+                </span>
+                <p>{product.description}</p>
+                <span>{product.id}</span>
+              </div>
 
-            <div>
-                <Link to="/">
-                    <button>Back to Products</button>
-                </Link>
+              <button
+                  className={`add_to_cart_btn_d ${disabledProducts.includes(product.id) ? 'disabled' : ''}`}
+                  onClick={() => handleAddToCart(product)}
+                  disabled={disabledProducts.includes(product.id)}
+              >
+                  {disabledProducts.includes(product.id) ? <span>Added to Cart</span> : 'Add to Cart'}
+              </button>
+
+              <div className='b2p_btn_div'>
+                  <Link to="/">
+                      <button>Back to Products</button>
+                  </Link>
+              </div>
             </div>
         </div>
         
